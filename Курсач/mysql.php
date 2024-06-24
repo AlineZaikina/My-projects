89ee08b8b76789246e839553d168bf8cb8c7d959
AlineZaikina <160338703+AlineZaikina@users.noreply.github.com>
2024-06-24 22:03:46 +0300

    Delete Курсач directory

--- Курсач/mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'inc.php'; 
-$mysqli = new mysqli($host, $user, $password, $db);
-/*if ($mysqli->connect_errno) 
-echo "Не удалось подключиться к MySQL: " . $mysqli->connect_error;
-else
-	echo "Mysql ok";*/
-$mysqli->set_charset("utf8")
-?>
