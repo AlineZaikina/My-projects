421ed7d350bc99f90edae4f9c58df74066271a8e
AlineZaikina <160338703+AlineZaikina@users.noreply.github.com>
2024-07-03 17:21:45 +0300

    Add files via upload

---
+++ Dinner/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Dinner</title>
+</head>
+<body>
+    <nav class="nav">
+      <li class="li"><a class="btn btn-outline-primary nav__link" href="menu-settings.php" role="button">Сформировать меню</a></li>
+      <li class="li"><a class="btn btn-outline-primary nav__link" href="#" role="button">Меню на эту неделю</a></li>
+      <li class="li"><a class="btn btn-outline-primary nav__link" href="#" role="button">Отчеты</a></li>
+      <li class="li"><a class="btn btn-outline-primary nav__link" href="#" role="button">Настройки</a></li>
+    </nav>
